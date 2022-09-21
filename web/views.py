b8109f7b9d4e5d08a72d94fe1d4149f01edfb2d4
Michael Murwayi <mikemurwayi7@gmail.com>
2022-09-21 11:59:03 +0300

    ft-super-admin:Add required routes and views for super admin dashboard

--- web/views.py
+++ web/views.py
@@ -3,5 +3,26 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 
+# Super admin views
+# This views are restricted only to Super admin role users
+
 class DashboardView(TemplateView):
     template_name = "dashboard.html"
+
+class SuperAdminCompanyView(TemplateView):
+    pass
+
+class SuperAdminSaccoView(TemplateView):
+    pass
+
+class SuperAdminUsersView(TemplateView):
+    pass
+
+class SuperAdminChartsView(TemplateView):
+    pass
+
+class SuperAdminEventsView(TemplateView):
+    pass
+
+class SuperAdminMapsView(TemplateView):
+    pass
